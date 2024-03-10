c5f5a5931eb2544737bcc444752b27e8f0f73cc7
Sumin Lee <suminn0707@gmail.com>
2024-03-10 17:23:24 +0900

    fix: 일부 구조체 순서 변경, feat: utils 함수 추가 및 execdata 구조체 수정

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 20:09:37 by sumilee           #+#    #+#             */
-/*   Updated: 2024/03/10 14:00:28 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/03/10 17:14:02 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 
-typedef struct s_pipe
-{
-	t_token			*addr;
-	struct s_pipe	*next;
-}				t_pipe;
-
 typedef struct s_token
 {
 	char			*str;
 	int				type;
 	struct s_token	*next;
 }				t_token;
+
+typedef struct s_pipe
+{
+	t_token			*addr;
+	struct s_pipe	*next;
+}				t_pipe;
 
 typedef struct s_env
 {
@@ -49,6 +49,13 @@ typedef struct s_execdata
 	pid_t	pid;
 	char	**path;
 	int		status;
-}				t_execdata
+	t_pipe	pipe;
+	t_env	env;
+}				t_execdata;
+
+void	error_exit(char *msg, char *cmd, char *arg, int code);
+void	*ft_malloc_err(size_t size);
+char	*ft_strdup_err(const char *s1);
+
 
 #endif
