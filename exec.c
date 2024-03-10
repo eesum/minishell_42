a0036bc3f1990947c789f6b018d2a6d542e3670e
Sumin Lee <suminn0707@gmail.com>
2024-03-10 17:24:00 +0900

    feat: envp를 링크드리스트(t_env)로 변환하는 함수 추가함

--- exec.c
+++ exec.c
@@ -6,36 +6,16 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 13:38:45 by sumilee           #+#    #+#             */
-/*   Updated: 2024/03/10 16:32:27 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/03/10 17:17:36 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	error_exit(char *msg, char *cmd, char *arg, int code)
-{
-	if (msg != NULL)
-	{
-		ft_putstr_fd("pipex: ", 2);
-		if (cmd != NULL)
-		{
-			ft_putstr_fd(cmd, 2);
-			ft_putstr_fd(": ", 2);
-		}
-		if (arg != NULL)
-		{
-			ft_putstr_fd(arg, 2);
-			ft_putstr_fd(": ", 2);
-		}
-		ft_putstr_fd(msg, 2);
-		write(2, "\n", 1);
-	}
-	exit(code);
-}
 
+// void exec(t_execdata *data, )
+// {
 
-void exec()
-{
 	//here doc 실행
 		// heredoc 개수 확인
 	// heredoc 개수 1개 이상이면, 그 동안 while 돌면서 순서대로 limiter에 지금 단어 집어넣고
@@ -52,4 +32,38 @@ void exec()
 
 	// wait childe
 	// heredoc 파일들 다 지우기??????
+// }
+
+
+t_env *parse_envp(char **envp)
+{
+	t_env	*env;
+	t_env	*cur;
+	t_env	*new;
+	int		i;
+
+	i = 0;
+	env = ft_malloc_err(sizeof(t_env));
+	env->str = ft_strdup_err("?=");
+	env->next = NULL;
+	cur = env;
+	while (envp[i])
+	{
+		new = ft_malloc_err(sizeof(t_env));
+		new->str = ft_strdup_err(envp[i]);
+		new->next = NULL;
+		cur->next = new;
+		cur = cur->next;
+		i++;
+	}
+	return (env);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_env *env;
+
+	env = parse_envp(envp);
+
+	return (0);
 }
