7aa46e4c03f07a521e020e917e344f5d717688c8
Sumin Lee <suminn0707@gmail.com>
2024-03-11 13:01:41 +0900

    libft 추가

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumilee <sumilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 12:55:44 by sumilee           #+#    #+#             */
+/*   Updated: 2023/10/12 18:39:47 by sumilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*cur;
+
+	if (*lst != NULL)
+	{
+		cur = ft_lstlast(*lst);
+		cur->next = new;
+	}
+	else
+		*lst = new;
+}
