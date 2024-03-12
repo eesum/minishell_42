8ab39fb1f16164b150ea82abfdac737d68f771b8
Sumin Lee <suminn0707@gmail.com>
2024-03-12 16:00:09 +0900

    fix: cd 리팩토링. to_directory로 중복 코드(함수)를 하나의 함수로 합쳤고,  에러 메세지 표시 방식이 변경됨

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 20:14:04 by sumilee           #+#    #+#             */
-/*   Updated: 2024/03/11 22:50:09 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/03/12 15:57:31 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,16 @@ void	change_dir(char *dest_path, char *current_path, char *arg, t_list *env)
 	update_env("PWD", dest_path, env);
 }
 
-void	to_oldpwd(char *current_path, char **cmd, t_list *env)
+void	to_directory(char *path, char *current_path, char **cmd, t_list *env)
 {
 	char	*dest_path;
 
-	dest_path = find_env("OLDPWD", env);
+	dest_path = find_env(path, env);
 	if (dest_path == NULL)
-		error_exit("OLDPWD not set", cmd[0], 0, 2);
+		error_exit("not set", cmd[0], path, 2);
 	change_dir(dest_path, current_path, 0, env);
-	printf("%s\n", dest_path);
-	free(dest_path);
-	return ;
-}
-
-void	to_home(char *current_path, char **cmd, t_list *env)
-{
-	char	*dest_path;
-
-	dest_path = find_env("HOME", env);
-	if (dest_path == NULL)
-		error_exit("HOME not set", cmd[0], 0, 2);
-	change_dir(dest_path, current_path, 0, env);
-	printf("%s\n", dest_path);
+	if (ft_memcmp(path, "OLDPWD", 7) == 0)
+		printf("%s\n", dest_path);
 	free(dest_path);
 	return ;
 }
@@ -61,7 +49,6 @@ void	exec_cd(char **cmd, t_list *env)
 	char	*dest_path;
 	char	tmp[3];
 
-	path = getcwd(NULL, 0);
 	if (cmd[1] && cmd[1][0] == '-' && cmd[1][1] != '\0')
 	{
 		tmp[0] = '-';
@@ -69,10 +56,11 @@ void	exec_cd(char **cmd, t_list *env)
 		tmp[2] = '\0';
 		error_exit("invalid option", cmd[0], tmp, 2);
 	}
-	else if (cmd[1] && cmd[1][0] == '-' && cmd[1][1] == '\0')
-		to_oldpwd(path, cmd, env);
+	path = getcwd(NULL, 0);
+	if (cmd[1] && cmd[1][0] == '-' && cmd[1][1] == '\0')
+		to_directory("OLDPWD", path, cmd, env);
 	else if (cmd[1] == NULL || cmd[1][0] == '~' && cmd[1][1] == '\0')
-		to_home(path, cmd, env);
+		to_directory("HOME", path, cmd, env);
 	else
 	{
 		dest_path = ft_strjoin_sep(path, cmd[1], "/");
@@ -80,26 +68,6 @@ void	exec_cd(char **cmd, t_list *env)
 		free(dest_path);
 	}
 	free(path);
-}
-
-t_list *envp_to_lst(char **envp)
-{
-	t_list	*env;
-	t_list	*new;
-	int		i;
-
-	i = 0;
-	env = ft_malloc_err(sizeof(t_list));
-	env->content = ft_strdup_err("?=0");
-	env->next = NULL;
-	while (envp[i])
-	{
-		new = ft_malloc_err(sizeof(t_list));
-		new->content = ft_strdup_err(envp[i]);
-		ft_lstadd_back(&env, new);
-		i++;
-	}
-	return (env);
 }
 
 // int	main(int argc, char **argv, char **envp)
@@ -112,8 +80,8 @@ t_list *envp_to_lst(char **envp)
 // 	char *cmd[3];
 
 // 	cmd[0] = "cd";
-// 	cmd[1] = NULL;
-// 	// cmd[2] = NULL;
+// 	cmd[1] = "~";
+// 	cmd[2] = NULL;
 // 	cur = data.env;
 // 	while (cur != NULL)
 // 	{
@@ -127,10 +95,6 @@ t_list *envp_to_lst(char **envp)
 // 	{
 // 		printf("%s\n", cur->content);
 // 		cur = cur->next;
-// 	}
-// 	while (1) {
-// 		system("pwd");
-// 		return 0;
 // 	}
 // 	return (0);
 // }
