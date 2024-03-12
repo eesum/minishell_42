3c0c3aab3d41fd61ff101845ab516768cbc1dacc
Sumin Lee <suminn0707@gmail.com>
2024-03-12 16:52:26 +0900

    fix: cd 추가 수정

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 20:14:04 by sumilee           #+#    #+#             */
-/*   Updated: 2024/03/12 15:57:31 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/03/12 16:41:57 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	change_dir(char *dest_path, char *current_path, char *arg, t_list *env)
 {
+	char	*new_pwd;
+
 	if (chdir(dest_path) < 0)
 	{
 		if (errno == EACCES || errno == EFAULT)
@@ -25,8 +27,10 @@ void	change_dir(char *dest_path, char *current_path, char *arg, t_list *env)
 		else
 			error_exit("Error occured", "cd", arg, 2);
 	}
+	new_pwd = getcwd(NULL, 0);
 	update_env("OLDPWD", current_path, env);
-	update_env("PWD", dest_path, env);
+	update_env("PWD", new_pwd, env);
+	free(new_pwd);
 }
 
 void	to_directory(char *path, char *current_path, char **cmd, t_list *env)
@@ -57,10 +61,10 @@ void	exec_cd(char **cmd, t_list *env)
 		error_exit("invalid option", cmd[0], tmp, 2);
 	}
 	path = getcwd(NULL, 0);
-	if (cmd[1] && cmd[1][0] == '-' && cmd[1][1] == '\0')
-		to_directory("OLDPWD", path, cmd, env);
-	else if (cmd[1] == NULL || cmd[1][0] == '~' && cmd[1][1] == '\0')
+	if (cmd[1] == NULL) // || ft_memcmp("~", cmd[1], 2) ==0 이거만 뺄지 아래것도 뺄지..!
 		to_directory("HOME", path, cmd, env);
+	else if (ft_memcmp("-", cmd[1], 2) == 0)
+		to_directory("OLDPWD", path, cmd, env);
 	else
 	{
 		dest_path = ft_strjoin_sep(path, cmd[1], "/");
@@ -76,19 +80,14 @@ void	exec_cd(char **cmd, t_list *env)
 // 	t_list *cur;
 	
 // 	data.env = envp_to_lst(envp);
-	
-// 	char *cmd[3];
 
-// 	cmd[0] = "cd";
-// 	cmd[1] = "~";
-// 	cmd[2] = NULL;
 // 	cur = data.env;
 // 	while (cur != NULL)
 // 	{
 // 		printf("%s\n", cur->content);
 // 		cur = cur->next;
 // 	}
-// 	exec_cd(cmd, data.env);
+// 	exec_cd(&argv[1], data.env);
 	
 // 	cur = data.env;
 // 	while (cur != NULL)
