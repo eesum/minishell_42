52c59196d0b90222e5e32affdeb33899f2fe36af
Sumin Lee <suminn0707@gmail.com>
2024-03-16 20:20:12 +0900

    refactor: 중복 코드 리팩토링

--- exec_child_process.c
+++ exec_child_process.c
@@ -6,18 +6,16 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 13:32:08 by sumilee           #+#    #+#             */
-/*   Updated: 2024/03/16 19:05:24 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/03/16 19:25:33 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	input_fd(t_execdata *data, int i)
+int	input_fd(t_execdata *data, t_list *cur_pipe, int i)
 {
 	int	file_fd;
-	t_list	*cur_pipe;
 
-	cur_pipe = ft_findlst_by_index(data->pipe, i);
 	file_fd = open_last_input(cur_pipe, data->file_arr);
 	if (file_fd < 0)
 	{
@@ -31,12 +29,10 @@ int	input_fd(t_execdata *data, int i)
 	return (file_fd);
 }
 
-int	output_fd(t_execdata *data, int i)
+int	output_fd(t_execdata *data, t_list *cur_pipe, int i)
 {
 	int	file_fd;
-	t_list	*cur_pipe;
 
-	cur_pipe = ft_findlst_by_index(data->pipe, i);
 	file_fd = open_last_output(cur_pipe);
 	if (file_fd < 0)
 	{
@@ -65,8 +61,9 @@ void	exec_in_child(t_execdata *data, int i)
 		exit(EXIT_FAILURE);
 	if (data->index < data->pipe_cnt - 1)
 		close(data->fd[i % 2][0]);
-	input = input_fd(data, i);
-	output = output_fd(data, i);
+	input = input_fd(data, cur_pipe, i);
+	output = output_fd(data, cur_pipe, i);
 	dup_fds(data, input, output);
 	exec_cmd(cmd, data->env);
+	exit(EXIT_SUCCESS);
 }
