56f2e4ec57c94b63123df1d49b88cf6da0673ad7
Sumin Lee <suminn0707@gmail.com>
2024-03-18 12:43:46 +0900

    feat: echo -n 옵션 작동방식 수정

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,48 +6,67 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:44:34 by sumilee           #+#    #+#             */
-/*   Updated: 2024/03/13 21:44:07 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/03/18 12:42:56 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	check_echo_option(char **cmd, int *n_flag)
+int	flag_echo_option(char *cmd)
 {
 	int	i;
+	int	flag;
 
-	if (cmd[1][0] == '-')
+	if (cmd[0] == '-')
 	{
 		i = 1;
-		while (cmd[1][i])
+		while (cmd[i])
 		{
-			if (cmd[1][i] == 'n')
-				*n_flag = 1;
+			if (cmd[i] == 'n')
+				flag = 1;
 			else
 			{
-				*n_flag = 0;
+				flag = 0;
 				break;
 			}
 			i++;
 		}
 	}
+	return (flag);
+}
+
+int	check_echo_option(char **cmd, int *n_flag)
+{
+	int	i;
+	int	j;
+
+	if (flag_echo_option(cmd[1]) == 1)
+		*n_flag = 1;
+	i = 2;
+	while (cmd[i])
+	{
+		if (flag_echo_option(cmd[i]) == 0)
+			return (i);
+		i++;
+	}
+	return (0);
 }
 
 int	exec_echo(char **cmd)
 {
 	int	i;
 	int	n_flag;
+	int	content_index;
 
 	n_flag = 0;
 	if (cmd[1] != NULL)
 	{
-		check_echo_option(cmd, &n_flag);
-		i = 1 + n_flag;
-		while (cmd[i])
+		content_index = check_echo_option(cmd, &n_flag);
+		while (cmd[content_index])
 		{
-			printf("%s", cmd[i]);
-			i++;
-			if (cmd[i])
+			printf("%s", cmd[content_index]);
+			content_index++;
+			if (cmd[content_index])
 				printf(" ");
 		}
 		if (n_flag == 0)
